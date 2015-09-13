67c1b23d2badb73b3e378327ddb27c4bbb05467f
timeraider@gmx.at <timeraider@gmx.at>
2015-09-13 22:55:08 +0200

    changed SuppressWarningsProcessor

--- plugins/org.eclipse.xtext.xtext.generator/src/org/eclipse/xtext/xtext/generator/parser/antlr/postProcessing/SuppressWarningsProcessor.java
+++ plugins/org.eclipse.xtext.xtext.generator/src/org/eclipse/xtext/xtext/generator/parser/antlr/postProcessing/SuppressWarningsProcessor.java
@@ -7,12 +7,18 @@
  *******************************************************************************/
 package org.eclipse.xtext.xtext.generator.parser.antlr.postProcessing;
 
+@Deprecated
+/***
+ * we do not need this class any more as the suppress warnings is already
+ * added by newer ANTLR3 versions itself (see ANTLR changes)
+ */
 public class SuppressWarningsProcessor {
 
 	public String process(String content) {
-		String result = content.replaceFirst("public class ", "@SuppressWarnings(\"all\")\n" +
-				"public class ");
-		return result;
+		// we do not need this any more 
+		// String result = content.replaceFirst("public class ", "@SuppressWarnings(\"all\")\n" +
+		//		"public class ");
+		return content;
 	}
 
 }
